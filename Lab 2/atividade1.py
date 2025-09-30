622c4a4a29ec901d63fa761e8a0417fe113e467e
HenriqueSigwalt <140031473+HenriqueSigwalt@users.noreply.github.com>
2025-09-30 12:08:43 -0300

    Lab 2 - Atividade 2 - Incompleto

--- Lab 2/atividade1.py
+++ Lab 2/atividade1.py
@@ -20,7 +20,9 @@ Gi=np.float32(I1[:,:,1])
 Bi=np.float32(I1[:,:,0])
 
 dist=((Bi-B)**2 + (Gi-G)**2 + (Ri-R)**2)**(1/2)
-index=(dist<30)
+index=(dist<50)
+I2=np.zeros([linhas,colunas],np.uint8)
+I2[index]=255
 
 h=1
 w=2*h+1
@@ -32,33 +34,32 @@ target=np.zeros([w,w,3],np.float32)
 target[:,:]=[160,160,180]
 
 ax1.clear()
-ax1.imshow(cv2.cvtColor(I1, cv2.COLOR_BGR2RGB))
+ax1.imshow(cv2.cvtColor(I2, cv2.COLOR_BGR2RGB))
 Inova=cv2.imread("Lab 2\Moodle\Tatoo1.jpg")
 
 for i in range(linhas-h):
     for j in range(colunas-h):
         if(i>82 and i<560) and (j>255 and j<425):
-            if(True in index[i-h:i+h+1,j-h:j+h+1]):
-                O=I1[i-h:i+h+1,j-h:j+h+1]
-                O[O==0]=1
-                dist=target[:,:,0]-O[:,:,0]
-                sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-                target[:,:,0]=O[sup[0],sup[1],0]
-                kernel=(target[:,:,0])/(O[:,:,0])
-                Inova[i,j,0]=np.average(O[:,:,0]*kernel)
-                dist=target[:,:,1]-O[:,:,1]
-                sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-                target[:,:,1]=O[sup[0],sup[1],1]
-                kernel=(target[:,:,1])/(O[:,:,1])
-                Inova[i,j,1]=np.average(O[:,:,1]*kernel)
-                dist=target[:,:,2]-O[:,:,2]
-                sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-                target[:,:,2]=O[sup[0],sup[1],2]
-                kernel=(target[:,:,2])/(O[:,:,2])
-                Inova[i,j,2]=np.average(O[:,:,2]*kernel)
-                #print("resultado")
-                #print(dist)
-                #print(O[:,:,0])
+            O=I1[i-h:i+h+1,j-h:j+h+1]
+            O[O==0]=1
+            dist=target[:,:,0]-O[:,:,0]
+            sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
+            target[:,:,0]=O[sup[0],sup[1],0]
+            kernel=(target[:,:,0])/(O[:,:,0])
+            Inova[i,j,0]=np.average(O[:,:,0]*kernel)
+            dist=target[:,:,1]-O[:,:,1]
+            sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
+            target[:,:,1]=O[sup[0],sup[1],1]
+            kernel=(target[:,:,1])/(O[:,:,1])
+            Inova[i,j,1]=np.average(O[:,:,1]*kernel)
+            dist=target[:,:,2]-O[:,:,2]
+            sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
+            target[:,:,2]=O[sup[0],sup[1],2]
+            kernel=(target[:,:,2])/(O[:,:,2])
+            Inova[i,j,2]=np.average(O[:,:,2]*kernel)
+            #print("resultado")
+            #print(dist)
+            #print(O[:,:,0])
 
 ax2.clear()
 ax2.imshow(cv2.cvtColor(Inova, cv2.COLOR_BGR2RGB))
---
+++ Lab 2/atividade2.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+
+def canny(I1,sigma,ti,ts):
+    linhas, colunas = I1.shape
+    w=int(6*sigma+1)
+    kernel = np.zeros((w,w), np.float32)
+
+    v1 = 1/(2 * np.pi * (sigma**2))
+    v2 = -1/(2 * (sigma**2))
+
+    for y in np.arange(0,w):
+        for x in np.arange(0,w):
+
+            # corrige coordenadas para a fun¸c~ao gaussiana
+            j = y - (w-1)/2
+            i = x - (w-1)/2
+
+            kernel[y,x] = v1 * np.exp(v2*(j**2 + i**2))
+
+            kernel = kernel/np.sum(kernel)
+    I1=cv2.filter2D(I1,-1,kernel, borderType=cv2.BORDER_REFLECT101)
+
+    grad_X, grad_y=np.gradient(I1.astype(float))
+    grad_mag=np.sqrt(grad_X**2+grad_y**2)
+    grad_dir=np.arctan2(grad_y,grad_X)* 180 / np.pi
+
+    I2=np.zeros([linhas,colunas],np.uint8)
+    Is=np.zeros([linhas,colunas],np.uint8)
+    Ii=np.zeros([linhas,colunas],np.uint8)
+    Ib=np.zeros([linhas,colunas],np.uint8)
+
+    grad_mag=((grad_mag/(np.max(grad_mag)+1e-10))*255).astype(np.uint8)
+
+    for i in range(1,linhas-1):
+        for j in range(1,colunas-1):
+            theta=grad_dir[i,j]
+            mag=grad_mag[i,j]
+            #print(theta)
+            #print(teste[i,j])
+            if ((theta < -22.5) and (theta>-67.5) or (theta < 157.5) and (theta>112.5)):
+                u=grad_mag[i+1,j+1]
+                v=grad_mag[i-1,j-1]
+            elif ((theta < -112.5) and (theta>-157.5) or (theta < 67.5) and (theta>22.5)):
+                u=grad_mag[i+1,j-1]
+                v=grad_mag[i-1,j+1]
+            elif ((theta < -67.5) and (theta>-112.5) or (theta < 112.5) and (theta>67.5)):
+                u=grad_mag[i+1,j]
+                v=grad_mag[i-1,j]
+            else:
+                u=grad_mag[i,j-1]
+                v=grad_mag[i,j+1]
+            if (u<=mag)and(v<=mag):
+                I2[i,j]=mag
+
+    Is[I2>ts]=I2[I2>ts]
+    Ii[I2>ti]=I2[I2>ti]
+    Ii=Ii-Is
+    for i in range(linhas):
+        for j in range(colunas):
+            if(Is[i,j]>0):
+                Ib[i-1:i+2,j-1:j+2]=Ii[i-1:i+2,j-1:j+2]
+    Ib=Ib+Is
+
+    teste=Ii
+    
+    return Ib,teste
+
+I1=cv2.imread("Lab 2\Moodle\castle.jpg",cv2.IMREAD_GRAYSCALE)
+w=3
+sigma=(w-1)/6
+ti=30
+ts=80
+
+Icanny=cv2.Canny(I1,50,150)
+
+Ib, teste=canny(I1,sigma,ti,ts)
+
+"""fig = plt.figure()
+ax1 = fig.add_subplot(1,3,1)
+ax2 = fig.add_subplot(1,3,2)
+ax3 = fig.add_subplot(1,3,3)
+
+ax1.clear()
+ax1.imshow(Icanny, cmap='gray')
+
+ax2.clear()
+ax2.imshow(Ib, cmap='gray')
+
+ax3.clear()
+ax3.imshow(teste, cmap='gray')"""
+
+plt.figure()
+plt.imshow(Ib, cmap='gray')
+#plt.figure()
+#plt.imshow(teste, cmap='gray')
+
+plt.show()
