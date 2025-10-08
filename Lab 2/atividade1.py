ac9b7c755cafa10eef760e79b34f0b2e39a7bb28
HenriqueSigwalt <140031473+HenriqueSigwalt@users.noreply.github.com>
2025-10-08 17:27:38 -0300

    Comentarios (Tomara que de para entender agora)

--- Lab 2/atividade1.py
+++ Lab 2/atividade1.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
 
+#Carrega a Imagem
 I1=cv2.imread("Lab 2\Moodle\Tatoo1.jpg")
 
 fig = plt.figure()
@@ -11,56 +12,52 @@ ax2 = fig.add_subplot(1,2,2)
 
 linhas, colunas, camadas = I1.shape
 
-R=140
-G=110
-B=100
-
-Ri=np.float32(I1[:,:,2])
-Gi=np.float32(I1[:,:,1])
-Bi=np.float32(I1[:,:,0])
-
-dist=((Bi-B)**2 + (Gi-G)**2 + (Ri-R)**2)**(1/2)
-index=(dist<50)
-I2=np.zeros([linhas,colunas],np.uint8)
-I2[index]=255
-
 h=1
 w=2*h+1
 
 #y = 82 - 560    x = 255 - 425
 
+#inicializa o kernel
 kernel=np.zeros([w,w],np.float32)
-target=np.zeros([w,w,3],np.float32)
-target[:,:]=[160,160,180]
+#cria uma matriz "Alvo" com a cor da pele
+tar=np.zeros([w,w,3],np.float32)
+tar[:,:]=[160,160,180]
 
+#Plota a imagem original
 ax1.clear()
-ax1.imshow(cv2.cvtColor(I2, cv2.COLOR_BGR2RGB))
+ax1.imshow(cv2.cvtColor(I1, cv2.COLOR_BGR2RGB))
 Inova=cv2.imread("Lab 2\Moodle\Tatoo1.jpg")
 
+#Percorre todos os pixels, exceto os da borda da imagem, aplicando a filtragem
 for i in range(linhas-h):
     for j in range(colunas-h):
+        #Se estiver na região em torno da imagem
         if(i>82 and i<560) and (j>255 and j<425):
+            #Pega a região em torno do pixel a ser analisado
             O=I1[i-h:i+h+1,j-h:j+h+1]
             O[O==0]=1
-            dist=target[:,:,0]-O[:,:,0]
+            #Calcula a distancia da cor de cada pixel da área para a cor do "alvo"
+            dist=tar[:,:,0]-O[:,:,0]
+            #Pega o index da menor distancia e define ela como a cor desejada
             sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-            target[:,:,0]=O[sup[0],sup[1],0]
-            kernel=(target[:,:,0])/(O[:,:,0])
+            target=O[sup[0],sup[1],0]
+            #Cria um Kernel que leva a cor dos pixels para a cor desejada
+            kernel=(target)/(O[:,:,0])
+            #Aplica o Kernel na imagem
             Inova[i,j,0]=np.average(O[:,:,0]*kernel)
-            dist=target[:,:,1]-O[:,:,1]
+            #Repete o processo para cada uma das camadas de cor da imagem
+            dist=tar[:,:,1]-O[:,:,1]
             sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-            target[:,:,1]=O[sup[0],sup[1],1]
-            kernel=(target[:,:,1])/(O[:,:,1])
+            target=O[sup[0],sup[1],1]
+            kernel=(target)/(O[:,:,1])
             Inova[i,j,1]=np.average(O[:,:,1]*kernel)
-            dist=target[:,:,2]-O[:,:,2]
+            dist=tar[:,:,2]-O[:,:,2]
             sup=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
-            target[:,:,2]=O[sup[0],sup[1],2]
-            kernel=(target[:,:,2])/(O[:,:,2])
+            target=O[sup[0],sup[1],2]
+            kernel=(target)/(O[:,:,2])
             Inova[i,j,2]=np.average(O[:,:,2]*kernel)
-            #print("resultado")
-            #print(dist)
-            #print(O[:,:,0])
 
+#Mostra a imagem suavizada
 ax2.clear()
 ax2.imshow(cv2.cvtColor(Inova, cv2.COLOR_BGR2RGB))
 
--- Lab 2/atividade2.py
+++ Lab 2/atividade2.py
@@ -3,11 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib
 
+#Função de encontrar bordas
 def canny(I1,sigma,ti,ts):
     linhas, colunas = I1.shape
+    #Define tamanho do kernel
     w=int(6*sigma+1)
     kernel = np.zeros((w,w), np.float32)
-
+    #Aplicação do kernel gaussiano
     v1 = 1/(2 * np.pi * (sigma**2))
     v2 = -1/(2 * (sigma**2))
 
@@ -23,25 +25,33 @@ def canny(I1,sigma,ti,ts):
             kernel = kernel/np.sum(kernel)
     I1=cv2.filter2D(I1,-1,kernel, borderType=cv2.BORDER_REFLECT101)
 
-    grad_X=cv2.Sobel(I1,cv2.CV_64F,1,0,ksize=w)
-    grad_y=cv2.Sobel(I1,cv2.CV_64F,0,1,ksize=w)
+    # Derivada em relação a x (Sobel)
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    grad_X = cv2.filter2D(I1, cv2.CV_32F, Kx, borderType=cv2.BORDER_REFLECT101)
 
+    # Derivada em relação a y (Sobel)
+    Ky = Kx.T
+    grad_y = cv2.filter2D(I1, cv2.CV_32F, Ky, borderType=cv2.BORDER_REFLECT101)
+
+    #Magnitude do gradiente
     grad_mag=np.sqrt(grad_X**2+grad_y**2)
+    #fase do gradiente
     grad_dir=np.arctan2(grad_y,grad_X)* 180 / np.pi
 
+    #Imagem
     I2=np.zeros([linhas,colunas],np.uint8)
     Is=np.zeros([linhas,colunas],np.uint8)
     Ii=np.zeros([linhas,colunas],np.uint8)
     Ib=np.zeros([linhas,colunas],np.uint8)
 
+    #Normaliza a magnitude (Não lembro o pq)
     grad_mag=((grad_mag/(np.max(grad_mag)+1e-10))*255).astype(np.uint8)
 
     for i in range(1,linhas-1):
         for j in range(1,colunas-1):
             theta=grad_dir[i,j]
             mag=grad_mag[i,j]
-            #print(theta)
-            #print(teste[i,j])
+            #Determina quais são os visinhos com base no intervalo onde estão os 
             if ((theta < -22.5) and (theta>-67.5) or (theta < 157.5) and (theta>112.5)):
                 u=grad_mag[i+1,j+1]
                 v=grad_mag[i-1,j-1]
@@ -54,49 +64,42 @@ def canny(I1,sigma,ti,ts):
             else:
                 u=grad_mag[i,j-1]
                 v=grad_mag[i,j+1]
+            #Se o pixel atual tiver gradiente maior que os vizinhos coloca ele na imagem I2
             if (u<=mag)and(v<=mag):
                 I2[i,j]=mag
 
+    #Faz a limiarização superior da imagem
     Is[I2>ts]=I2[I2>ts]
+    #Faz a limiarização inferior da imagem
     Ii[I2>ti]=I2[I2>ti]
+    #Calcula a diferença das duas limiarizações
     Ii=Ii-Is
+    
+    #Percorre toda a imagem superior
     for i in range(linhas):
         for j in range(colunas):
+            #Se o pixel foi identificado como borda
             if(Is[i,j]>0):
+                #Defini todos os pixel em volta dele como borda
                 Ib[i-1:i+2,j-1:j+2]=Ii[i-1:i+2,j-1:j+2]
+    #Defini todos os pixels da imagem superior como borda
     Ib=Ib+Is
 
-    teste=Ii
-    
-    return Ib,teste
+    #Retorna as imagens geradas
+    return Ib
 
 I1=cv2.imread("Lab 2\Moodle\castle.jpg",cv2.IMREAD_GRAYSCALE)
+#tamanho do kernel
 w=3
 sigma=(w-1)/6
-ti=30
-ts=80
 
-Icanny=cv2.Canny(I1,50,150)
+#Limiares inferiores e superiores
+ti=0.01
+ts=0.1
 
-Ib, teste=canny(I1,sigma,ti,ts)
-
-"""fig = plt.figure()
-ax1 = fig.add_subplot(1,3,1)
-ax2 = fig.add_subplot(1,3,2)
-ax3 = fig.add_subplot(1,3,3)
-
-ax1.clear()
-ax1.imshow(Icanny, cmap='gray')
-
-ax2.clear()
-ax2.imshow(Ib, cmap='gray')
-
-ax3.clear()
-ax3.imshow(teste, cmap='gray')"""
+#Realiza a detecção de bordas
+Ib=canny(I1,sigma,ti,ts)
 
 plt.figure()
 plt.imshow(Ib, cmap='gray')
-plt.figure()
-plt.imshow(teste, cmap='gray')
-
 plt.show()
